42ce907dd4b56e230e32d9de01a7a75075494c85
Gabriel <prismaticnetwork12@gmail.com>
2022-07-25 21:16:24 -0300

    feat: implementing firebase

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,4 +1,9 @@
+import { signInWithGooglePopup } from "../utils/firebase/firebase.utils";
 const Login = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
   return (
     <>
       <section className="login-form">
@@ -9,6 +14,9 @@ const Login = () => {
         <div className="password-info">
           <label>Password</label>
           <input type="password" name="Password" placeholder="Your password" />
+        </div>
+        <div className="log-in-with-google">
+          <button onClick={logGoogleUser}> Log in with GOOGLE </button>
         </div>
       </section>
     </>
